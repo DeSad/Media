42d1b2efa9549f9c7e7583e3775bf709acdc6b80
Stanislav <stas.kozhemyako@gmail.com>
2016-05-20 13:27:22 +0300

    Category
    TableRepresentation

---
+++ BlueLibrary/Catagory/Album+TableRepresentation.h
@@ -0,0 +1,13 @@
+//
+//  Album+TableRepresentation.h
+//  BlueLibrary
+//
+//  Created by Stanislav Kozhemyako on 5/20/16.
+//  Copyright © 2016 Eli Ganem. All rights reserved.
+//
+
+#import "Album.h"
+
+@interface Album (TableRepresentation)
+
+@end
---
+++ BlueLibrary/Catagory/Album+TableRepresentation.m
@@ -0,0 +1,13 @@
+//
+//  Album+TableRepresentation.m
+//  BlueLibrary
+//
+//  Created by Stanislav Kozhemyako on 5/20/16.
+//  Copyright © 2016 Eli Ganem. All rights reserved.
+//
+
+#import "Album+TableRepresentation.h"
+
+@implementation Album (TableRepresentation)
+
+@end
